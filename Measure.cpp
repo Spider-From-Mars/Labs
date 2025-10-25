731ee7a1446f7483736399c472acec316edd21ae
Pechorin <albermorser0@gmail.com>
2025-10-25 16:05:58 +0500

    Added new method to Measure class

--- Measure.cpp
+++ Measure.cpp
@@ -2,6 +2,11 @@
 
 Measure::Measure() : start{std::chrono::high_resolution_clock::now()} {}
 
+Measure::Measure(double seconds, long memory)
+    : start{std::chrono::high_resolution_clock::now()}, timeLimit{seconds}, memoryLimit{memory}
+{
+}
+
 void Measure::stop()
 {
     auto duration =
@@ -10,6 +15,20 @@ void Measure::stop()
     std::cout << "Затрачено времени: " << duration << " сек." << std::endl;
     std::cout << "Использовано памяти: " << getMemoryUsage() / 1024.f / 1024.f << " МБ"
               << std::endl;
+}
+
+void Measure::checkLimit()
+{
+    auto duration =
+        std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start).count();
+
+    if (duration > timeLimit)
+        throw std::runtime_error("Time limit exceeded");
+
+    auto memory = getMemoryUsage() / 1024.f / 1024.f;
+
+    if (memory > memoryLimit)
+        throw std::runtime_error("Memory limit exceeded");
 }
 
 long Measure::getMemoryUsage()
--- Measure.hpp
+++ Measure.hpp
@@ -1,17 +1,22 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <sys/resource.h>
 
 class Measure
 {
   public:
     Measure();
+    Measure(double seconds, long memory);
 
     void stop();
+    void checkLimit();
 
   private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    double timeLimit;
+    long memoryLimit;
 
     long getMemoryUsage();
 };
